525d922f1c45a59a952bbf3e72c4086fc1b7938c
Neef6 <wenzhe.li@intel.com>
2017-03-24 10:53:47 +0800

    2

--- src/cn/edu/nj/WordCount.scala
+++ src/cn/edu/nj/WordCount.scala
@@ -8,18 +8,10 @@ import org.apache.spark.SparkContext
 
 object WordCount {
   def main(args: Array[String]) {
-    if (args.length < 1) {
-      System.err.println("Usage: <file>")
-      System.exit(1)
-    }
-
     val conf = new SparkConf()
     val sc = new SparkContext("local","wordcount",conf)
-    val line = sc.textFile(args(0))
-
+    val line = sc.textFile("/Users/apple/Documents/inputPath/word.txt")
     line.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_+_).collect().foreach(println)
-
     sc.stop()
   }
 }
-
